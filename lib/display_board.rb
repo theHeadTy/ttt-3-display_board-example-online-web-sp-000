463e0caaf77c518f8549a295be2846d86836a77a
generic-puppet-7755 <tasmith1990@gmail.com>
2019-09-27 22:03:42 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
 def display_board
-  Array.new(3)
-  Array.new(3)
+  while < 2 puts Array.new(3)
+  puts "\n"
+  while < 2 puts Array.new(3)
 end
